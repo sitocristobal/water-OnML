58ac658956dd2f7285138bb02dec83824476245f
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-03-04 22:22:26 +0100

    Improved joiner

--- joiner.py
+++ joiner.py
@@ -1,49 +1,26 @@
-import os
-import numpy as np
+import glob
 import pandas as pd
-
-"""This function joins several csv files which contain time series of the
-same lenght into a unique csv file."""
-
-# NEEDSWORK: this function is too dependent on the file names. This can be improved I think
 
 def joiner():
     
-    # Set the names of the files which contain the data
-    amonio = "ammonium_nor.csv"
-    conductividad = "conductivity_nor.csv"
-    nivel = "water level_nor.csv"
-    oxigeno = "dissolved oxygen_nor.csv"
-    ph = "pH_nor.csv"
-    temperatura = "water temperature_nor.csv"
-    turbidez = "turbidity_nor.csv"
+    # List all csv files in /data
+    csv_files = glob.glob('data/*_nor.csv')
+    
+    # Initialize an empty dataframe to store the merged data
+    merged_df = pd.read_csv(csv_files[0], sep=';')
+    first_var = str(merged_df.columns[1])
+    
+    # Loop through each file and append to the merged dataframe
+    for i in csv_files[1:]:
 
-    # Read the files and store them as a pandas database
-    df = pd.read_csv(f'data/{amonio}', delimiter=';', parse_dates=['date'], index_col=['date'])
-    dfConductividad = pd.read_csv(f'data/{conductividad}', delimiter=';', parse_dates=['date'], index_col=['date'])
-    dfNivel = pd.read_csv(f'data/{nivel}', delimiter=';', parse_dates=['date'], index_col=['date'])
-    dfOxigeno = pd.read_csv(f'data/{oxigeno}', delimiter=';', parse_dates=['date'], index_col=['date'])
-    dfph = pd.read_csv(f'data/{ph}', delimiter=';', parse_dates=['date'], index_col=['date'])
-    dfTemperatura = pd.read_csv(f'data/{temperatura}', delimiter=';', parse_dates=['date'], index_col=['date'])
-    dfTurbidez = pd.read_csv(f'data/{turbidez}', delimiter=';', parse_dates=['date'], index_col=['date'])
+        # Read the CSV file and store it in a temporary dataframe
+        df = pd.read_csv(i, sep=';')
+        
+        cols = list(df.columns)
+        cols.pop(1)
+        merged_df = pd.merge(merged_df, df, on=cols)
+        # print(merged_df)
 
-
-    # Extract the desired columns
-    conductividad = dfConductividad['value']
-    nitratos = dfNivel['value']
-    oxigeno = dfOxigeno['value']
-    ph = dfph['value']
-    temperatura = dfTemperatura['value']
-    turbidez = dfTurbidez['value']
-
-    # Insert the new columns in the database
-    df.insert(1, "conductivity", conductividad, True)
-    df.insert(3, "oxygen", oxigeno, True)
-    df.insert(4, "pH", ph, True)
-    df.insert(6, "turbidity", turbidez, True)
-    df.insert(2, "water level", nitratos, True)
-    df.insert(5, "water temperature", temperatura, True)
-
-    cols = list(df.columns.values.tolist())
-    cols = [i.replace("value", "ammonium") for i in cols]
-    df.to_csv(f'data/joined.csv', sep=';', encoding='utf-8', index=True, header=cols)
+    # Save the merged dataframe to a new CSV file
+    merged_df.insert(9, first_var, merged_df.pop(first_var))
+    merged_df.to_csv('data/merged.csv', sep=';', encoding='utf-8', index=False)
--- preprocessing.py
+++ preprocessing.py
@@ -1,37 +1,36 @@
 import os
 
 from checkGaps import checkGaps
-from normalizer import normalizer
+from columner import columner
 from joiner import joiner
 from mfilterer import mfilterer
-
 
 # Define the data we want to study
 files = [f for f in os.listdir("data") if os.path.isfile(os.path.join("data", f))]
 
-varNames = [i[0:-4] for i in files] # extract the names of the variables
+varNames = [i[0:-4] for i in files] # Extract the names of the variables
 
-# Define the time dram we want to use (a: months, b: weeks, c: days)
+# Define the time frame we want to use (a: months, b: weeks, c: days)
 timeFrame = 'b'
 timeStep = '15 min'
 
 
 if __name__ == '__main__':
 
-    # for varName in varNames:
+    for varName in varNames:
         
-    #     # Fill in the gaps in the time series
-    #     checkGaps(File=f'{varName}.csv', timestep=timeStep)
-    #     print('[INFO] checkGaps() DONE')
+        # Fill in the gaps in the time series
+        checkGaps(File=f'{varName}.csv', timestep=timeStep)
+        print('[INFO] checkGaps() DONE')
 
-    #     # Normalize the data. See normalizer.py for details
-    #     normalizer(File=f'{varName}_full.csv', timeframe=timeFrame, timestep=timeStep)
-    #     print('[INFO] normalizer() DONE')
+        # Add time-related columns to the data. See columner.py for details
+        columner(File=f'{varName}_full.csv', timeframe=timeFrame, timestep=timeStep)
+        print('[INFO] normalizer() DONE')
         
     
     # Join the normalized databases
-    joiner()
-    print('[INFO] joiner() DONE')
+    # joiner()
+    # print('[INFO] joiner() DONE')
 
     # Filter out those months or weeks or days (depending on the desired
     # time unit) with too many NaN in several variables and iterate on the rest
